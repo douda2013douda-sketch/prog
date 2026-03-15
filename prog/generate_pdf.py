99ca65e26950b393dcc3597d5719028bf111f093
اسمك <email@example.com>
2026-03-15 01:03:43 +0000

    modified:   prog/generate_pdf.py modified:   prog/leave_template.tex

--- prog/generate_pdf.py
+++ prog/generate_pdf.py
@@ -20,23 +20,20 @@ class PDFGenerator:
     def generate(self, employee, leave):
 
         with open(self.template_path, "r", encoding="utf-8") as f:
-            template = Template(f.read())
+            template_content = f.read()
 
-        # حساب الـ slash
+        # حساب المتغيرات
         slash1 = "" if employee.title == "سجل غمد" else "\\slash"
         slash2 = "" if employee.title == "سامط بف" else "\\slash"
         slash3 = "" if employee.title == "سامط" else "\\slash"
 
-        # حساب الـ type
         type1 = "" if leave.leave_type == "تمديد اجازة" else "\\type"
         type2 = "" if leave.leave_type == "نقاهة" else "\\type"
         type3 = "" if leave.leave_type == "عطلة" else "\\type"
         type4 = "" if leave.leave_type == "إجازة" else "\\type"
 
-        data = {
-            "employee": employee,
-            "leave": leave,
-            # المتغيرات الجديدة
+        # قاموس المتغيرات
+        variables = {
             "leave_num": leave.document_number(),
             "national_id": employee.national_id,
             "department": employee.department,
@@ -44,24 +41,28 @@ class PDFGenerator:
             "name": employee.last_name,  # اللقب
             "last_name": employee.first_name,  # الإسم
             "job": employee.job,
-            "duration": leave.duration(),
-            "start_date": leave.start_date,
+            "duration": str(leave.duration()),
+            "start-date": leave.start_date,
             "end_date": leave.end_date,
             "direction": employee.direction,
             "signature_date": leave.signature_date,
-            "slash1": slash1,
-            "slash2": slash2,
-            "slash3": slash3,
+            "SLASH1": slash1,
+            "SLASH2": slash2,
+            "SLASH3": slash3,
             "type1": type1,
             "type2": type2,
             "type3": type3,
             "type4": type4,
         }
 
+        # استبدال \VAR{var} بقيم
+        for var, value in variables.items():
+            template_content = template_content.replace(f"\\VAR{{{var}}}", str(value))
+
         tex_file = "temp.tex"
 
         with open(tex_file, "w", encoding="utf-8") as f:
-            f.write(template.render(data))
+            f.write(template_content)
 
         subprocess.run(["xelatex", "-interaction=nonstopmode", tex_file])
 
